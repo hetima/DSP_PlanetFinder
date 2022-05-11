d69acab1022e6fb1e3dd305764fb6ac2a9d793a9
hetima <hetima@hetima.com>
2022-05-11 23:27:26 +0900

    refactor

---
+++ PlanetFinder/DSPStarMapMemoIntg.cs
@@ -0,0 +1,71 @@
+﻿using System;
+//using System.Text;
+using System.Reflection;
+using BepInEx;
+using System.Collections.Generic;
+
+namespace PlanetFinderMod
+{
+    public class DSPStarMapMemoIntg
+    {
+        public readonly bool canGetSignalIconId;
+        private FieldInfo _memoPool;
+        private FieldInfo _signalIconIdField;
+
+        public DSPStarMapMemoIntg()
+        {
+            canGetSignalIconId = false;
+            try
+            {
+                Dictionary<string, PluginInfo> plugins = BepInEx.Bootstrap.Chainloader.PluginInfos;
+                if (plugins.TryGetValue("Appun.DSP.plugin.StarMapMemo", out PluginInfo pluginInfo) && pluginInfo.Instance != null)
+                {
+                    Type classType = pluginInfo.Instance.GetType().Assembly.GetType("DSPStarMapMemo.MemoPool");
+                    Type classType2 = pluginInfo.Instance.GetType().Assembly.GetType("DSPStarMapMemo.MemoPool+Memo");
+                    if (classType != null && classType2 != null)
+                    {
+                        _signalIconIdField = classType2.GetField("signalIconId");
+                        _memoPool = classType.GetField("memoPool", BindingFlags.Public | BindingFlags.Static);
+                    }
+                    canGetSignalIconId = (_memoPool != null);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public int GetSignalIconId(int planetId)
+        {
+            if (_memoPool != null && _signalIconIdField != null)
+            {
+                object d = _memoPool.GetValue(null);
+                if (d == null)
+                {
+                    return 0;
+                }
+                object[] arg = new object[] { planetId, null };
+                bool flag = (bool)d.GetType().InvokeMember("TryGetValue", BindingFlags.InvokeMethod, null, d, arg);
+                if (flag && arg.Length == 2 && arg[1] != null)
+                {
+                    object signalIconIdObj = _signalIconIdField.GetValue(arg[1]);
+                    if (signalIconIdObj != null)
+                    {
+                        int[] signalIconId = signalIconIdObj as int[];
+                        for (int i = 0; i < signalIconId.Length; i++)
+                        {
+                            if (signalIconId[i] != 0)
+                            {
+                                return signalIconId[i];
+                            }
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+    }
+}
+
---
+++ PlanetFinder/LSTMIntg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+//using System.Text;
+using System.Reflection;
+using BepInEx;
+using System.Collections.Generic;
+
+namespace PlanetFinderMod
+{
+    public class LSTMIntg
+    {
+        public MethodInfo _openPlanetId;
+
+        public readonly bool canOpenPlanetId;
+
+        public LSTMIntg()
+        {
+            canOpenPlanetId = false;
+            _openPlanetId = null;
+            try
+            {
+                Dictionary<string, PluginInfo> plugins = BepInEx.Bootstrap.Chainloader.PluginInfos;
+                if (plugins.TryGetValue("com.hetima.dsp.LSTM", out PluginInfo pluginInfo) && pluginInfo.Instance != null)
+                {
+                    _openPlanetId = pluginInfo.Instance.GetType().GetMethod("IntegrationOpenPlanetId", new[] { typeof(int) });
+                    canOpenPlanetId = (_openPlanetId != null);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void OpenPlanetId(int planetId)
+        {
+            if (_openPlanetId != null)
+            {
+                object[] param = { planetId };
+                _openPlanetId.Invoke(null, param);
+            }
+        }
+    }
+}
+

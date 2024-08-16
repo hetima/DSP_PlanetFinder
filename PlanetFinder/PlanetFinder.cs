42667b73725980bf487e36f2080784f06f0e4fbd
hetima <33700+hetima@users.noreply.github.com>
2024-08-16 09:52:09 +0900

    v1.1.4

--- PlanetFinder/PlanetFinder.cs
+++ PlanetFinder/PlanetFinder.cs
@@ -13,7 +13,7 @@ using static PlanetFinderMod.UIPlanetFinderWindow;
 namespace PlanetFinderMod
 {
 
-    [BepInPlugin(__GUID__, __NAME__, "1.1.3")]
+    [BepInPlugin(__GUID__, __NAME__, "1.1.4")]
     public class PLFN : BaseUnityPlugin
     {
         public const string __NAME__ = "PlanetFinder";

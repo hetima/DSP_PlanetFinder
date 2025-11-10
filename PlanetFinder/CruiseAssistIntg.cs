50935b609fef8fac81d369fb1a8ae1c0d221aa83
hetima <33700+hetima@users.noreply.github.com>
2025-11-10 11:08:39 +0900

    Support CAAP_N

--- PlanetFinder/CruiseAssistIntg.cs
+++ PlanetFinder/CruiseAssistIntg.cs
@@ -9,6 +9,7 @@ namespace PlanetFinderMod
     public class CruiseAssistIntg
     {
         public MethodInfo _selectPlanetOrStar;
+        public MethodInfo _selectPlanetOrStar_CAAP_N;
 
         public readonly bool canSelectPlanet;
 
@@ -16,14 +17,22 @@ namespace PlanetFinderMod
         {
             canSelectPlanet = false;
             _selectPlanetOrStar = null;
+            _selectPlanetOrStar_CAAP_N = null;
             try
             {
+                PluginInfo pluginInfo;
                 Dictionary<string, PluginInfo> plugins = BepInEx.Bootstrap.Chainloader.PluginInfos;
-                if (plugins.TryGetValue("tanu.CruiseAssist", out PluginInfo pluginInfo) && pluginInfo.Instance != null)
+                if (plugins.TryGetValue("tanu.CruiseAssist", out pluginInfo) && pluginInfo.Instance != null)
                 {
                     Type classType = pluginInfo.Instance.GetType().Assembly.GetType("tanu.CruiseAssist.CruiseAssistStarListUI");
                     _selectPlanetOrStar = classType.GetMethod("SelectStar", new[] { typeof(StarData), typeof(PlanetData) });
                     canSelectPlanet = (_selectPlanetOrStar != null);
+                }
+                else if (plugins.TryGetValue("nord.CruiseAssist", out pluginInfo) && pluginInfo.Instance != null)
+                {
+                    Type classType = pluginInfo.Instance.GetType().Assembly.GetType("tanu.CruiseAssist.CruiseAssistStarListUI");
+                    _selectPlanetOrStar_CAAP_N = classType.GetMethod("SelectStar", new[] { typeof(StarData), typeof(PlanetData), typeof(bool) });
+                    canSelectPlanet = (_selectPlanetOrStar_CAAP_N != null);
                 }
             }
             catch (Exception)
@@ -34,7 +43,7 @@ namespace PlanetFinderMod
 
         public void SelectPlanetOrStar(PlanetData planet, StarData star = null)
         {
-            if (_selectPlanetOrStar == null)
+            if (_selectPlanetOrStar == null && _selectPlanetOrStar_CAAP_N == null)
             {
                 return;
             }
@@ -46,6 +55,11 @@ namespace PlanetFinderMod
             {
                 object[] param = { star, planet };
                 _selectPlanetOrStar.Invoke(null, param);
+            }
+            else if (_selectPlanetOrStar_CAAP_N != null && (planet != null || star != null))
+            {
+                object[] param = { star, planet, true };
+                _selectPlanetOrStar_CAAP_N.Invoke(null, param);
             }
         }
     }
